8c3a635a98766be55553c380423ed53bd60374b8
Mohammadsdq <mmdhsn855@gmail.com>
2023-07-22 15:44:58 +0330

    add DOM and setting

--- html/index.js
+++ html/index.js
@@ -13,12 +13,41 @@ $(document).ready(function () {
         let data = event.data
         let type = data.type
         if (!typeDatas[type]) return console.error("This type is not registered")
-        
+
     })
 });
 
 
 
-let iconName = "";
-let massage = "";
-let color = "";
+// new js 
+
+
+let addNotif = document.getElementById("allnotify");
+
+
+// show notif General setting
+let iconName = "success";
+let title = "Success"
+let massage = "This is a exampel alert for test and debug";
+let color = "#8dfd9e";
+
+
+function showNotfiyE() {
+    const newNotify = document.createElement("div");
+    newNotify.innerHTML = `
+    <div class="notif" style="border-left: 5px solid ${color};">
+        <div class="content">
+            <img src="./images/${iconName}.png" style="background-color: ${color};">
+            <div class="text-area">
+                <h1>${title}</h1>
+                <p id="showmsg">${massage}</p>
+            </div>
+        </div>
+        <div class="loader"></div>
+    </div>
+    `;
+    addNotif.appendChild(newNotify)
+
+};
+
+showNotfiyE()
