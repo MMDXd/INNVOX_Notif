98377c3a710d30b9294d5d45b8859bb6429ba3d0
XdMMD <102690832+MMDXd@users.noreply.github.com>
2023-08-02 19:04:10 +0430

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,6 @@
-lua54 "yes"
-client_script '@F-AC/antiInjection/client.lua'
-server_script '@F-AC/antiInjection/server.lua'
-
 fx_version('cerulean')
 games({ 'gta5' })
+version "2.1"
 author "INNVOX Team"
 client_scripts({
     "client.lua"
